5ae76b0bb09333839b9fd3af5c22feb70bce5309
Benjamin Lane <benjamin.lane@incodegame.com>
2020-06-12 21:07:44 -0700

    Updates Yojimbo to use monobehavior based approach.

---
+++ Assets/Scripts/Networking/Yojimbo/NetworkPlayerState.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkPlayerState : MonoBehaviour
+{
+    public ulong Id;
+    public ulong MoveState;
+    public ulong State;
+    public Vector3 Pos;
+    public float Scale;
+    public bool IsOwner;
+}
---
+++ Assets/Scripts/Networking/Yojimbo/PlayerUpdateSystem.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Client;
+using UnityEngine;
+
+public class PlayerUpdateSystem : MonoBehaviour
+{
+    public Dictionary<ulong, NetworkPlayerState> playerNetworkDictionary;
+
+    void Awake()
+    {
+
+    }
+
+    public void ProcessPlayerUpdates()
+    {
+
+    }
+
+    void Update()
+    {
+        if (playerNetworkDictionary == null || playerNetworkDictionary.Count <= 0) { return; }
+
+        foreach (KeyValuePair<ulong, NetworkPlayerState> entry in playerNetworkDictionary)
+        {
+            if (entry.Value.IsOwner)
+            {
+                LocalPlayer localPlayer = entry.Value.GetComponent<LocalPlayer>();
+                localPlayer.transform.position = entry.Value.Pos;
+            }
+            else
+            {
+                NetworkPlayer networkPlayer = entry.Value.GetComponent<NetworkPlayer>();
+                networkPlayer.transform.position = entry.Value.Pos;
+            }
+        }
+    }
+}
